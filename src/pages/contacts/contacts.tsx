18c2471c282825ff967a0393bc2ba8a1e67ff8fe
didi <didilina25@gmail.com>
2022-07-12 20:06:53 +0900

    contacts page first commit

--- src/pages/contacts/contacts.tsx
+++ src/pages/contacts/contacts.tsx
@@ -1,9 +1,18 @@
 import React from "react";
+import { Link } from "react-router-dom";
+
+import whatsapp from '../../images/logos_whatsapp.svg'
 
 export const Contacts = () => {
     return (
         <div>
-            
+            <p>lorem ipsum....</p>
+            <div>
+                <Link to="https://wa.clck.bar/79142667993">
+                    <img src={whatsapp} alt="whatsapp"/>
+                    <p>+7(914)266-79-93</p>
+                </Link>
+            </div>
         </div>
     )
 }
